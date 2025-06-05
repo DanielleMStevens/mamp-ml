bfee154409b0676b57c09d600eebdcd6d73bf8f2
Brian Su <briansuaccs@email.com>
2025-06-04 18:29:06 -0700

    fix pipeline bug?

--- prepare_input_data.sh
+++ prepare_input_data.sh
@@ -32,16 +32,12 @@ run_script() {
     local log_file
     if [[ $script == *.R ]]; then
         log_file="logs/$(basename "$script" .R).R.log"
-    else
-        log_file="logs/$(basename "$script" .py).log"
     fi
     
     echo "Running $script..."
     
     if [[ $script == *.R ]]; then
         Rscript "scripts/$script" "$@" 2>&1 | tee "$log_file"
-    else
-        python "scripts/$script" "$@" 2>&1 | tee "$log_file"
     fi
     
     if [ ${PIPESTATUS[0]} -ne 0 ]; then
@@ -55,38 +51,11 @@ run_script() {
 
 
 # Run scripts in order
-#echo "Starting MAMP prediction pipeline..."
-#echo "----------------------------------------"
+echo "Starting MAMP prediction pipeline..."
+echo "----------------------------------------"
 
 # create intermediate files for model prediction
-#mkdir -p intermediate_files
-#run_script "01_convert_sheet_to_fasta.R" "$INPUT_FILE"
-
-# run AlphaFold locally to extract ectodomin of receptor sequence
-echo "Running AlphaFold to model the receptor sequence..."
+echo "Creating fasta file for AlphaFold modeling..."
+mkdir -p intermediate_files
 mkdir -p intermediate_files/receptor_only
-export PATH="/Users/briansu/workspace/mamp-ml/localcolabfold/colabfold-conda/bin:$PATH"
-conda activate localfold
-colabfold_batch --num-models 1 ./intermediate_files/receptor_full_length.fasta ./intermediate_files/receptor_only/
-
-# run LRR-Annotation to extract LRRs from receptor sequence
-#echo "Running LRR-Annotation to extract LRRs from receptor sequence..."
-#mkdir -p intermediate_files/lrr_annotation_plots
-#mkdir -p intermediate_files/pdb_for_lrr_annotator
-#run_script "02_alphafold_to_lrr_annotation.py"
-
-
-
-#run_script "03_parse_lrr_annotations.py"
-#run_script "04_chemical_conversion.R"
-#run_script "05_data_prep_for_training.py"
-#echo "Data preparation completed successfully!" 
-#conda activate mamp_prediction
-
-
-
-
-
-
-
-
+run_script "01_convert_sheet_to_fasta.R" "$INPUT_FILE"
