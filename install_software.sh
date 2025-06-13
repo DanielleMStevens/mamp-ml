f63ed444e803dc37bca879da94788822180fc333
Brian Su <briansuaccs@email.com>
2025-06-13 15:41:17 -0700

    update software install

--- install_software.sh
+++ install_software.sh
@@ -5,3 +5,7 @@ mamba update --all
 mamba install cuda -c nvidia
 bash scripts/install_colabbatch_linux.sh
 
+conda deactivate
+conda create --name env_esm.ym
+conda env create -f env_esm.yml
+conda env update -f env_esm.yml
