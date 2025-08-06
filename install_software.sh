e9c7fad00b116f3c85c03de0f95255d15d16880b
Danielle M. Stevens <45525389+DanielleMStevens@users.noreply.github.com>
2025-08-06 14:37:05 -0700

    Potential bug fix for install software

--- install_software.sh
+++ install_software.sh
@@ -1,11 +1,28 @@
-conda create --name=localfold python=3.11 -y
+# install conda packages
+conda create --name=localfold python=3.11
 conda activate localfold
-conda install mamba 
+
+conda install mamba
 mamba update --all
 mamba install cuda -c nvidia
-bash scripts/install_colabbatch_linux.sh
+wget https://raw.githubusercontent.com/YoshitakaMo/localcolabfold/main/install_colabbatch_linux.sh
+bash install_colabbatch_linux.sh
 
+pip install matplotlib-inline
+conda install -c conda-forge wxpython -y
 conda deactivate
-conda create --name env_esm.ym
-conda env create -f env_esm.yml
-conda env update -f env_esm.yml
+
+bash /content/mamp-ml/scripts/install_esmfold_env.sh
+
+# --------- old code based from during development ------------
+#conda create --name=localfold python=3.11 -y
+#conda activate localfold
+#conda install mamba 
+#amba update --all
+#mamba install cuda -c nvidia
+#bash scripts/install_colabbatch_linux.sh
+
+#conda deactivate
+#conda create --name env_esm.ym
+#conda env create -f env_esm.yml
+#conda env update -f env_esm.yml
